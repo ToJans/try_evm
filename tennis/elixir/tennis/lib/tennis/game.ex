0b88302031c4c631a725b6f657ab4c7a8bf367e4
Bjoern Rochel <bjoern.rochel@xing.com>
2013-10-30 13:12:23 +0100

    Getting rid of the records in the elixir example

--- tennis/elixir/tennis/lib/tennis/game.ex
+++ tennis/elixir/tennis/lib/tennis/game.ex
@@ -1,20 +1,17 @@
-defrecord Game, for: nil
-defrecord Advantage, for: nil
-
 defmodule Tennis.Game do
 
   def score(player, game_state), do: next_game_state(player, game_state)
 
-  defp next_game_state(_, state = Game[for: _]),  do: state
+  defp next_game_state(_, state = [game: _]),  do: state
 
-  defp next_game_state(player, :deuce),  do: Advantage.new(for: player)
+  defp next_game_state(player, :deuce),  do: [advantage: player]
 
-  defp next_game_state(player, Advantage[for: other_player]) when player != other_player, do: :deuce
+  defp next_game_state(player, [advantage: other_player]) when player != other_player, do: :deuce
 
-  defp next_game_state(player, Advantage[for: _]), do: Game.new(for: player)
+  defp next_game_state(player, [advantage: _]), do: [game: player]
 
-  defp next_game_state(:playerA, [playerA: x, playerB: y]) when x == 40 and y <= 30, do: Game.new(for: :playerA)
-  defp next_game_state(:playerB, [playerA: x, playerB: y]) when x <= 30 and y == 40, do: Game.new(for: :playerB)
+  defp next_game_state(:playerA, [playerA: x, playerB: y]) when x == 40 and y <= 30, do: [game: :playerA]
+  defp next_game_state(:playerB, [playerA: x, playerB: y]) when x <= 30 and y == 40, do: [game: :playerB]
 
   defp next_game_state(:playerB, [playerA: x, playerB: y]) when x == 40 and y == 30, do: :deuce 
   defp next_game_state(:playerA, [playerA: x, playerB: y]) when x == 30 and y == 40, do: :deuce 

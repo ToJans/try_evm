a5301b610a579c35ad38b3a6bef5b1e627992c48
Bjoern Rochel <bjoern.rochel@xing.com>
2013-10-30 13:06:55 +0100

    Refactored elixir example to look more like the erlang version

--- tennis/elixir/tennis/lib/tennis/game.ex
+++ tennis/elixir/tennis/lib/tennis/game.ex
@@ -13,28 +13,18 @@ defmodule Tennis.Game do
 
   defp next_game_state(player, Advantage[for: _]), do: Game.new(for: player)
 
-  defp next_game_state(player, [playerA: x, playerB: y])
-    when x == 40 and y <= 30 and player == :playerA or
-         x <= 30 and y == 40 and player == :playerB do
-    Game.new(for: player)
-  end
+  defp next_game_state(:playerA, [playerA: x, playerB: y]) when x == 40 and y <= 30, do: Game.new(for: :playerA)
+  defp next_game_state(:playerB, [playerA: x, playerB: y]) when x <= 30 and y == 40, do: Game.new(for: :playerB)
 
-  defp next_game_state(player, [playerA: x, playerB: y])
-    when x == 40 and y == 30 and player == :playerB or
-         x == 30 and y == 40 and player == :playerA do
-    :deuce
-  end
+  defp next_game_state(:playerB, [playerA: x, playerB: y]) when x == 40 and y == 30, do: :deuce 
+  defp next_game_state(:playerA, [playerA: x, playerB: y]) when x == 30 and y == 40, do: :deuce 
 
   defp next_game_state(player, game_state = [playerA: _, playerB: _]) do
     old_score = Keyword.get(game_state, player)
     Keyword.put(game_state, player, new_player_score(old_score)) |> Enum.sort
   end
 
-  defp new_player_score(score) do
-    case score do
-      0  -> 15
-      15 -> 30
-      30 -> 40
-    end
-  end
+  defp new_player_score(0), do: 15
+  defp new_player_score(15), do: 30
+  defp new_player_score(30), do: 40
 end
